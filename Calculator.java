f3256da5bfd72ed6ad8b0d596979aaeb182f60d0
truuuely <kmyc7030@gmail.com>
2025-07-04 09:44:32 +0900

    feat: modify cal2

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator  {
-    public int add(String str1, String str2) {
-        int a = Integer.parseInt(str1);
-        int b = Integer.parseInt(str2);
+    public int add(String s1, String s2) {
+        int a = Integer.parseInt(s1);
+        int b = Integer.parseInt(s2);
         return a + b;
     }
 }
